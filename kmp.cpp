b6f0b20b792a621196d9c5826742eb73ef687386
Avan14 <145996250+Avan14@users.noreply.github.com>
2025-10-05 22:51:18 +0530

    code-10

---
+++ kmp.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string text, pattern;
+    cin >> text >> pattern;
+    int n = text.size();
+    int m = pattern.size();
+    vector<int> lps(m, 0);
+    // Preprocess the pattern to create the LPS array
+    // Standard KMP LPS preprocessing
+    lps[0] = 0;
+    int len = 0;
+    int i = 1;
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+     i= 0 ;
+     int j = 0;
+     while( i < text.size()){
+        if(pattern[j] == text[i]){
+            j++;
+            i++;
+            if(j==m){
+                cout<<"found " <<i - m<<"\n";
+            }
+        }
+        else if(  j == 0 ){
+            i++;
+        }
+        else {
+            j = lps[j-1];
+        }
+     }
+    
+
+  
+
+    return 0;
+}
